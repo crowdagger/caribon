35b94a522d6feb1c14d1e495d17ebc989c05f816
Elisabeth Henry <liz.henry@ouvaton.org>
2015-12-11 17:49:12 +0100

    word.rs: rewrote math as if let when possible

--- src/lib/word.rs
+++ src/lib/word.rs
@@ -35,21 +35,15 @@ pub enum Word {
 impl Word {
     /// Sets the stemmed value of a word.
     pub fn set_stemmed(&mut self, s: String) {
-        match self {
-            &mut Word::Tracked(_, ref mut stemmed, _, _) => {
-                *stemmed = s;
-            }
-            _ => {}
+        if let &mut Word::Tracked(_, ref mut stemmed, _, _) = self {
+            *stemmed = s;
         }
     }
 
     /// Sets the repetition value of a word.
     pub fn set_count(&mut self, x: f32) {
-        match self {
-            &mut Word::Tracked(_, _, ref mut v, _) => {
-                *v = x;
-            }
-            _ => {}
+        if let &mut Word::Tracked(_, _, ref mut v, _) = self {
+            *v = x;
         }
     }
 }
@@ -141,6 +135,6 @@ impl Ast {
                 }
             }
         }
-        return &mut self.words
+        &mut self.words
     }
 }
